cdd80ef290aa674f741626d646d7d32554301040
FL1NEE <70909941+FL1NEE@users.noreply.github.com>
2025-03-24 16:11:55 +0300

    Update titan_node.sh

--- titan_node.sh
+++ titan_node.sh
@@ -31,6 +31,13 @@ fi
 # Настройка прокси
 setup_proxy
 
+# Проверка ProxyChains
+if ! command -v proxychains &> /dev/null; then
+    echo -e "${BLUE}Установка ProxyChains...${NC}"
+    sudo apt update
+    sudo apt install proxychains -y
+fi
+
 # Проверка Docker
 if command -v docker &> /dev/null; then
     echo -e "${GREEN}Docker уже установлен. Пропускаем установку.${NC}"
@@ -45,49 +52,6 @@ else
     echo -e "${GREEN}Docker успешно установлен!${NC}"
 fi
 
-# Проверка Docker Compose
-if command -v docker-compose &> /dev/null; then
-    echo -e "${GREEN}Docker Compose уже установлен. Пропускаем установку.${NC}"
-else
-    echo -e "${BLUE}Устанавливаем Docker Compose...${NC}"
-    VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
-    sudo curl -L "https://github.com/docker/compose/releases/download/$VER/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
-    echo -e "${GREEN}Docker Compose успешно установлен!${NC}"
-fi
-
-# Добавление пользователя в группу Docker
-if ! groups $USER | grep -q '\bdocker\b'; then
-    echo -e "${BLUE}Добавляем текущего пользователя в группу Docker...${NC}"
-    sudo groupadd docker
-    sudo usermod -aG docker $USER
-else
-    echo -e "${GREEN}Пользователь уже находится в группе Docker.${NC}"
-fi
-
-# Загрузка Docker-образа Titan
-echo -e "${BLUE}Загружаем Docker-образ Titan...${NC}"
-docker pull nezha123/titan-edge
-
-# Создание директории Titan
-mkdir -p ~/.titanedge
-
-# Запуск Titan
-echo -e "${BLUE}Запускаем контейнер Titan...${NC}"
-docker run --name titan --network=host -d -v ~/.titanedge:/root/.titanedge nezha123/titan-edge
-
-# Привязка кода идентификации
-echo -e "${YELLOW}Введите ваш Titan identity code:${NC}"
-read identity_code
-docker run --rm -it -v ~/.titanedge:/root/.titanedge nezha123/titan-edge bind --hash="$identity_code" https://api-test1.container1.titannet.io/api/v2/device/binding
-
-# Заключительный вывод
-echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-echo -e "${YELLOW}Команда для проверки логов:${NC}"
-echo "docker logs -f titan"
-echo -e "${PURPLE}-----------------------------------------------------------------------${NC}"
-echo -e "${GREEN}CRYPTO FORTOCHKA — вся крипта в одном месте!${NC}"
-echo -e "${CYAN}Наш Telegram https://t.me/cryptoforto${NC}"
-
-# Проверка логов
-docker logs -f titan
+# Проверка занятости порта 1234
+if sudo netstat -tuln | grep -q ':1234'; then
+    echo -e "${RED}Порт 1234 уже занят. Пожалуйста, освободите пор
